886d447452391f066027536f086dc67266c6aee1
Ved <vednpattar03@gmail.com>
2025-03-31 10:59:56 +0530

    Switch to bcryptjs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const collection = require("./config");
 const { name } = require('ejs');
 
