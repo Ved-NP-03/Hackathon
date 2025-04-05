4ef66d13b48bf627a8339ffd371feb1759825c42
Ved <vednpattar03@gmail.com>
2025-04-05 11:47:53 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -51,6 +51,10 @@ app.get("/",(req,res) =>{
     res.render("login");
 });
 
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
 app.get("/signup",(req,res) =>{
     res.render("signup");
 });
